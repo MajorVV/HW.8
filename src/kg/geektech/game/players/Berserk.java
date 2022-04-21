d1f1be26c0f21c675e5c4e003151c38232cd813b
MajorVV <stabvtc@gmail.com>
2022-04-21 23:25:40 +0600

    Initial commit

---
+++ src/kg/geektech/game/players/Berserk.java
@@ -0,0 +1,23 @@
+package kg.geektech.game.players;
+
+import kg.geektech.game.general.RPG_Game;
+
+public  class Berserk extends Hero {
+    public Berserk(int health, int damage, String name) {
+        super(health, damage, SuperAbility.SAVE_DAMAGE_AND_REVERT, name);
+    }
+
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+        int block = RPG_Game.random.nextInt(50) + 1;
+        this.setHealth(this.getHealth() + block);
+        boss.setHealth(boss.getHealth() - block);
+        System.out.println("Берсеерк Поглотил: [" + block + "] damage!");
+    }
+
+    @Override
+    public void applySuperPower(Hero[] heroes, Boss boss) {
+
+    }
+}
